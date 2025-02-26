106ca2945d5532f43e94da5736a54ed63369037c
kavinda-100 <kavindarathnayake100@gmail.com>
2025-02-26 11:29:16 +0530

    add Categories component; implement category loading and error handling

--- src/app/(home)/products/ProductPage.tsx
+++ src/app/(home)/products/ProductPage.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import SearchBar from "./_components/search/SearchBar";
+import Categories from "./_components/categories/Categories";
+import { Skeleton } from "../../../components/ui/skeleton";
 
 const ProductPage = () => {
   return (
     <section className={"container mx-auto mt-4 flex flex-col gap-3"}>
       {/* Header */}
       <div className={"flex items-center justify-between gap-6"}>
-        <h1 className={"text-lg font-bold tracking-tight"}>
+        <h1
+          className={"text-lg font-bold tracking-tight text-muted-foreground"}
+        >
           Find Your <span className={"text-primary"}>Favorite</span> Foods
         </h1>
         <SearchBar />
       </div>
+      {/*  categories */}
+      <Categories />
     </section>
   );
 };
---
+++ src/app/(home)/products/_components/categories/Categories.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { useGetAllCategories } from "../../../../../hooks/api/categories/useGetAllCategories";
+import { CirclePlusIcon } from "lucide-react";
+import { Skeleton } from "../../../../../components/ui/skeleton";
+
+const Categories = () => {
+  const { data, isLoading, error } = useGetAllCategories();
+
+  return (
+    <div className="container mx-auto my-4">
+      <ScrollArea className="w-full whitespace-nowrap pb-4">
+        <div className="flex items-center gap-3">
+          {
+            // Loading State
+            isLoading && <Skeleton className={"h-[40px] w-full"} />
+          }
+          {
+            // Error State
+            error && (
+              <div className="flex items-center gap-2 rounded-md border border-gray-100 bg-red-100 px-4 py-2">
+                <div className="h-4 w-4 rounded-md bg-red-400"></div>
+                <span className="text-sm text-red-400">
+                  Failed to load categories
+                </span>
+              </div>
+            )
+          }
+          {data?.categories.map((category) => {
+            return (
+              <div
+                key={category.id}
+                className="group flex cursor-pointer items-center gap-2 rounded-md border border-gray-100 bg-muted/30 px-4 py-2 transition-all duration-300 ease-in-out hover:border-gray-300 dark:border-gray-800 dark:hover:border-gray-600"
+              >
+                {/* Circle Icon Inside the Div */}
+                <CirclePlusIcon className="size-4 scale-50 opacity-50 transition-all duration-300 ease-in-out group-hover:scale-100 group-hover:text-primary group-hover:opacity-100" />
+                <span className="text-sm text-gray-600 transition-colors duration-300 group-hover:text-primary dark:text-gray-400">
+                  {category.name}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default Categories;
