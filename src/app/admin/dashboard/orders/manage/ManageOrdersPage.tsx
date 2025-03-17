306e120090d6d2b6637c77f8261fcf131da6d7c3
kavinda-100 <kavindarathnayake100@gmail.com>
2025-03-17 12:05:36 +0530

    add orderId retrieval from URL query params in ManageOrdersPage

--- src/app/admin/dashboard/orders/manage/ManageOrdersPage.tsx
+++ src/app/admin/dashboard/orders/manage/ManageOrdersPage.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import React from "react";
+import { useSearchParams } from "next/navigation";
 
 const ManageOrdersPage = () => {
-  return <div>ManageOrdersPage</div>;
+  // Get the email from the URL query params.
+  const searchParams = useSearchParams();
+  const orderId = searchParams.get("orderId");
+
+  return <div>ManageOrdersPage - {orderId}</div>;
 };
 export default ManageOrdersPage;
