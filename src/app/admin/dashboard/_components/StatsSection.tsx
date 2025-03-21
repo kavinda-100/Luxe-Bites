19d656f1546ec474cfd3bd52452540bf385fbad2
kavinda-100 <kavindarathnayake100@gmail.com>
2025-03-21 11:22:58 +0530

    enhance StatsSection; implement StatsCard component with revenue statistics and percentage change display

--- src/app/admin/dashboard/_components/StatsSection.tsx
+++ src/app/admin/dashboard/_components/StatsSection.tsx
@@ -4,7 +4,19 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getAdminStatistics } from "../../../../actions/admin";
 import { Skeleton } from "../../../../components/ui/skeleton";
-import { LucideIcon } from "lucide-react";
+import {
+  ArrowDown01,
+  ArrowUp10,
+  CirclePlusIcon,
+  LucideIcon,
+} from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../../../components/ui/card";
 
 const StatsSection = () => {
   const { data, isLoading, error } = useQuery({
@@ -31,15 +43,90 @@ const StatsSection = () => {
 
   console.log(data);
 
-  return <section className={"container mx-auto"}>StatsSection</section>;
+  return (
+    <section className={"container mx-auto"}>
+      <div
+        className={
+          "grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3"
+        }
+      >
+        <StatsCard
+          label={"Revenue"}
+          total={data?.revenue.total ?? 0}
+          lastMonth={data?.revenue.lastMonth ?? 0}
+          thisMonth={data?.revenue.thisMonth ?? 0}
+          isIncreasing={data?.revenue.isIncreasing ?? false}
+          percentageChange={data?.revenue.percentageChange ?? 0}
+          icon={CirclePlusIcon}
+        />
+      </div>
+    </section>
+  );
 };
 export default StatsSection;
 
-type cardProrps = {
+type cardProps = {
+  label: string;
   total: number;
   lastMonth: number;
   thisMonth: number;
   isIncreasing: boolean;
   percentageChange: number;
   icon: LucideIcon;
+};
+
+const StatsCard = ({
+  label,
+  icon: Icon,
+  percentageChange,
+  total,
+  thisMonth,
+  lastMonth,
+  isIncreasing,
+}: cardProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className={"flex items-center gap-3"}>
+          <Icon className={"size-5 text-primary"} />
+          {label}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className={"flex justify-between gap-3"}>
+        <div className={"flex flex-col gap-3"}>
+          <p className={"font-mono text-3xl font-semibold"}>{total}</p>
+          <p className={"text-md font-normal"}>Total</p>
+        </div>
+
+        <div className={"text-md flex flex-col items-center gap-2 font-normal"}>
+          <p>
+            <span className={"font-mono text-lg font-semibold"}>
+              {thisMonth}
+            </span>{" "}
+            This month
+          </p>
+          {isIncreasing ? (
+            <ArrowUp10 className={"size-5 text-emerald-500"} />
+          ) : (
+            <ArrowDown01 className={"size-5 text-red-500"} />
+          )}
+          <p>
+            <span className={"font-mono text-lg font-semibold"}>
+              {lastMonth}
+            </span>{" "}
+            Last month
+          </p>
+        </div>
+      </CardContent>
+      <CardFooter>
+        <div className={"text-sm font-normal text-foreground/60"}>
+          {isIncreasing ? (
+            <p>Increased By {Math.floor(percentageChange)}%</p>
+          ) : (
+            <p>Decreased By {Math.floor(percentageChange)}%</p>
+          )}
+        </div>
+      </CardFooter>
+    </Card>
+  );
 };
