37591126452be8f496ccd78e70e479a3ab011248
kavinda-100 <kavindarathnayake100@gmail.com>
2025-03-21 10:16:45 +0530

    enhance OverviewPage; integrate right sidebar functionality and update lucide-react dependency

--- src/app/admin/dashboard/OverviewPage.tsx
+++ src/app/admin/dashboard/OverviewPage.tsx
@@ -2,8 +2,14 @@
 
 import React from "react";
 import StatsSection from "./_components/StatsSection";
+import { useRightSideBar } from "../../../store/useRightSideBar";
 
 const OverviewPage = () => {
+  const { close } = useRightSideBar();
+
+  React.useEffect(() => {
+    close();
+  }, [close]);
   return (
     <section className={"container mx-auto flex flex-col gap-4"}>
       <h1 className={"text-md font-medium text-foreground/80"}>Overview</h1>
