c75af79e07f5410cf239eb2cbc038d9d0c159b01
kavinda-100 <kavindarathnayake100@gmail.com>
2025-03-21 11:59:04 +0530

    add DemoChart component; implement interactive area chart for visitor data with customizable time range

--- src/app/admin/dashboard/OverviewPage.tsx
+++ src/app/admin/dashboard/OverviewPage.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import StatsSection from "./_components/StatsSection";
 import { useRightSideBar } from "../../../store/useRightSideBar";
+import { Component } from "../../../components/DemoChart";
 
 const OverviewPage = () => {
   const { close } = useRightSideBar();
@@ -12,8 +13,13 @@ const OverviewPage = () => {
   }, [close]);
   return (
     <section className={"container mx-auto flex flex-col gap-4"}>
+      {/* Overview */}
       <h1 className={"text-md font-medium text-foreground/80"}>Overview</h1>
       <StatsSection />
+
+      {/*Sales*/}
+      <h1 className={"text-md font-medium text-foreground/80"}>Sales</h1>
+      <Component />
     </section>
   );
 };
