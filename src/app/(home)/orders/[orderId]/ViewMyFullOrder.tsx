900e9a4c97afb4bb4abc1247bcc1af89e789feba
kavinda-100 <kavindarathnayake100@gmail.com>
2025-03-23 12:50:31 +0530

    add cancel order functionality to ViewMyFullOrder; implement reason input and loading state for cancellation

--- src/app/(home)/orders/[orderId]/ViewMyFullOrder.tsx
+++ src/app/(home)/orders/[orderId]/ViewMyFullOrder.tsx
@@ -6,10 +6,34 @@ import { getMyOrderById } from "../../../../actions/users/orders";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Skeleton } from "@/components/ui/skeleton";
-import { CheckCircle, XCircle } from "lucide-react";
+import { BanIcon, CheckCircle, Loader2, XCircle } from "lucide-react";
 import Image from "next/image";
+import { useCancelOrder } from "../../../../hooks/api/orders/useCancelOrder";
+import { toast } from "sonner";
+import { Label } from "../../../../components/ui/label";
+import { Textarea } from "../../../../components/ui/textarea";
+import { Button } from "../../../../components/ui/button";
 
 const ViewMyFullOrder = ({ orderId }: { orderId: string }) => {
+  const [cancelReason, setCancelReason] = React.useState("");
+  const { mutate, isPending } = useCancelOrder();
+
+  const handleCancelOrder = () => {
+    if (cancelReason.trim() === "") {
+      toast.error("Please provide a reason for canceling the order");
+      return;
+    }
+    mutate(
+      { orderId: orderId, cancelReason, cancelBy: "User" },
+      {
+        onSuccess: () => {
+          setCancelReason("");
+          window.location.reload();
+        },
+      },
+    );
+  };
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["getMyOrderById", { orderId }],
     queryFn: async () => getMyOrderById({ orderId }),
@@ -156,6 +180,35 @@ const ViewMyFullOrder = ({ orderId }: { orderId: string }) => {
       </Card>
 
       {/*  cancel the order */}
+      <div
+        className={
+          "container mx-auto mt-4 flex flex-col gap-4 rounded-md bg-muted/40 p-4"
+        }
+      >
+        <Label className={"text-md font-semibold"}>Cancel The order</Label>
+        <Textarea
+          placeholder={"Type the reason for canceling the order"}
+          className={"w-full"}
+          rows={3}
+          value={cancelReason}
+          onChange={(e) => setCancelReason(e.target.value)}
+        />
+        <Button
+          className={"w-fit"}
+          variant={"outline"}
+          onClick={handleCancelOrder}
+          disabled={isPending}
+        >
+          {isPending ? (
+            <Loader2 className={"size-3 animate-spin"} />
+          ) : (
+            <div className={"flex items-center gap-2"}>
+              <BanIcon className="h-4 w-4" />
+              Cancel
+            </div>
+          )}
+        </Button>
+      </div>
     </section>
   );
 };
